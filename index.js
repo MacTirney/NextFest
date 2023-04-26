483e55b9523d79e9d454436a5343e412b3a9d87d
Mac Tirney <mactirney@gmail.com>
2023-04-26 17:05:09 -0400

    Create the basic Express app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/',(req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`Serving on ${port}`)
+})
