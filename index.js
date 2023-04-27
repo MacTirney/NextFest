cbff4908b58bc26506c662b545b90460f8c09b64
Mac Tirney <mactirney@gmail.com>
2023-04-27 19:22:47 -0400

    Create festival model and integrate it into app

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const mongoose = require('mongoose');
+const Festival = require('./models/festival')
+
+mongoose.connect('mongodb://localhost:27017/My-Next-Fest');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -9,6 +19,12 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/',(req, res) => {
     res.render('home')
+})
+
+app.get('/makefestival', async (req, res) => {
+    const festival = new Festival({title: 'My Local Festival', description: 'Local festival with local artists' });
+    await festival.save();
+    res.send(festival)
 })
 
 app.listen(port, () => {
