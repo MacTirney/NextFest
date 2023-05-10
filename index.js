2000c65e60326286f2eb573e48020911f7853a74
Mac Tirney <mactirney@gmail.com>
2023-05-10 18:29:14 -0400

    Create an error handling class and async wrapper function

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Festival = require('./models/festival');
 const ejsEngine = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 
 mongoose.connect('mongodb://localhost:27017/My-Next-Fest');
 
@@ -26,46 +28,42 @@ app.get('/',(req, res) => {
     res.render('home')
 })
 
-app.get('/festivals', async (req, res) => {
+app.get('/festivals', catchAsync(async (req, res) => {
     const festivals = await Festival.find({})
     res.render('festivals/index', { festivals })
-})
+}))
 
 app.get('/festivals/new', (req, res) => {
     res.render('festivals/new')
 })
 
-app.post('/festivals', async (req, res, next) => {
-    try {
-        const festival = new Festival(req.body.festival)
-        await festival.save()
-        res.redirect(`/festivals/${festival._id}`)
-    } catch (e) {
-        next(e)
-    }
-})
+app.post('/festivals', catchAsync(async (req, res, next) => {
+    const festival = new Festival(req.body.festival)
+    await festival.save()
+    res.redirect(`/festivals/${festival._id}`)
+}))
 
-app.get('/festivals/:id', async (req, res) => {
+app.get('/festivals/:id', catchAsync(async (req, res) => {
     const festival = await Festival.findById(req.params.id)
     res.render('festivals/show', { festival })
-})
+}))
 
-app.get('/festivals/:id/edit', async (req, res) => {
+app.get('/festivals/:id/edit', catchAsync(async (req, res) => {
     const festival = await Festival.findById(req.params.id)
     res.render('festivals/edit', { festival })
-})
+}))
 
-app.put('/festivals/:id', async (req, res) => {
+app.put('/festivals/:id', catchAsync(async (req, res) => {
     const { id } = req.params
     const festival = await Festival.findByIdAndUpdate(id, { ...req.body.festival })
     res.redirect(`/festivals/${festival._id}`)
-})
+}))
 
-app.delete('/festivals/:id', async (req, res) => {
+app.delete('/festivals/:id', catchAsync(async (req, res) => {
     const { id } = req.params
     await Festival.findByIdAndDelete(id)
     res.redirect('/festivals')
-})
+}))
 
 app.use((err, req, res, next) => {
     res.send('Something went wrong')
