0c06d63e4a5bf421dfc7f556b3755a92fc2b8232
Mac Tirney <mactirney@gmail.com>
2023-06-06 13:52:18 -0400

    Set up public directory to serve static assets

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/festivals', festivals)
 app.use('/festivals/:id/reviews', reviews)
