1711dbea75e26a15eabbaf47047d862a58cca64e
Mac Tirney <mactirney@gmail.com>
2023-06-06 13:10:51 -0400

    Break out review routes

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const app = express();
 const path = require('path');
 const port = 3000;
 const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const Festival = require('./models/festival');
-const Review = require('./models/review')
 const ejsEngine = require('ejs-mate');
-const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
-const { festivalSchema, reviewSchema } = require('./schemas.js')
+const methodOverride = require('method-override');
 
 const festivals = require('./routes/festivals')
+const reviews = require('./routes/reviews')
 
 mongoose.connect('mongodb://localhost:27017/My-Next-Fest');
 
@@ -28,37 +25,12 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-
 app.use('/festivals', festivals)
+app.use('/festivals/:id/reviews', reviews)
 
 app.get('/',(req, res) => {
     res.render('home')
 })
-
-app.post('/festivals/:id/reviews', validateReview, catchAsync(async (req,res) => {
-    const festival = await Festival.findById(req.params.id)
-    const review = new Review(req.body.review)
-    festival.reviews.push(review)
-    await review.save()
-    await festival.save()
-    res.redirect(`/festivals/${festival._id}`)
-}))
-
-app.delete('/festivals/:id/reviews/:reviewId', catchAsync( async (req,res) => {
-    const { id, reviewId } = req.params
-    await Festival.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId)
-    res.redirect(`/festivals/${id}`)
-}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page not found', 404))
---
+++ routes/reviews.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const catchAsync = require('../utils/catchAsync')
+const ExpressError = require('../utils/ExpressError')
+const Festival = require('../models/festival');
+const Review = require('../models/review')
+const { reviewSchema } = require('../schemas.js')
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+router.post('/', validateReview, catchAsync(async (req,res) => {
+    const festival = await Festival.findById(req.params.id)
+    const review = new Review(req.body.review)
+    festival.reviews.push(review)
+    await review.save()
+    await festival.save()
+    res.redirect(`/festivals/${festival._id}`)
+}))
+
+router.delete('/:reviewId', catchAsync( async (req,res) => {
+    const { id, reviewId } = req.params
+    await Festival.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/festivals/${id}`)
+}))
+
+module.exports = router
