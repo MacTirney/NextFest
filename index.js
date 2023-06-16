81cfb636ec6b52286c82cbdfc7dc6d9a8a119ca1
Mac Tirney <mactirney@gmail.com>
2023-06-16 13:26:58 -0400

    Configure app to use Passport

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
 
 const festivals = require('./routes/festivals')
 const reviews = require('./routes/reviews')
@@ -41,6 +44,14 @@ const sessionConfig = {
 
 app.use(session(sessionConfig))
 app.use(flash())
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next) => {
     res.locals.success = req.flash('success')
