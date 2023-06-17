00b33f0c82404a32bfa673e54f9b81b1034f8933
Mac Tirney <mactirney@gmail.com>
2023-06-16 20:34:53 -0400

    Setup login routes and form

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
 const catchAsync = require('../utils/catchAsync')
+const passport = require('passport')
 
 router.get('/register', (req,res) => {
     res.render('users/register')
@@ -19,5 +20,14 @@ router.post('/register', catchAsync (async (req,res) => {
         res.redirect('/register');
     }
 }));
+
+router.get('/login', (req,res) => {
+    res.render('users/login')
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req,res) => {
+    req.flash('success', 'Welcome back');
+    res.redirect('/festivals')
+})
 
 module.exports = router;
