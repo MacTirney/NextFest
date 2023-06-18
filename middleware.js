7a8acafa105f8d15907b3cbee6226e1cd44d8a77
Mac Tirney <mactirney@gmail.com>
2023-06-18 14:48:40 -0400

    Setup logout route and middleware to confirm login

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req,res,next) => {
+    console.log('req.user:', req.user)
     if(!req.isAuthenticated()) {
         req.flash('error','You must be logged in')
         return res.redirect('/login')
--- routes/users.js
+++ routes/users.js
@@ -30,4 +30,14 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     res.redirect('/festivals')
 })
 
+router.get('/logout', (req,res) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err)
+        }
+        req.flash('success','You have successfully logged out')
+        res.redirect('/festivals')
+    });
+});
+
 module.exports = router;
