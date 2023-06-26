107f6dfdd9b79deac10695b4564679748e2fa2ce
Mac Tirney <mactirney@gmail.com>
2023-06-25 22:09:53 -0400

    Restructure festival routes

--- routes/festivals.js
+++ routes/festivals.js
@@ -5,18 +5,17 @@ const Festival = require('../models/festival');
 const { isLoggedIn, isAuthor, validateFestival } = require('../middleware')
 const festivals = require('../controllers/festivals')
 
-router.get('/', catchAsync(festivals.index))
+router.route('/')
+    .get(catchAsync(festivals.index))
+    .post(isLoggedIn, validateFestival, catchAsync(festivals.createFestival))
 
 router.get('/new', isLoggedIn, festivals.renderNewForm)
 
-router.post('/', isLoggedIn, validateFestival, catchAsync(festivals.createFestival))
-
-router.get('/:id', catchAsync(festivals.showFestival))
+router.route('/:id')
+    .get(catchAsync(festivals.showFestival))
+    .put(isLoggedIn, isAuthor, validateFestival, catchAsync(festivals.updateFestival))
+    .delete(isLoggedIn, isAuthor, catchAsync(festivals.deleteFestival))
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(festivals.renderEditForm))
-
-router.put('/:id', isLoggedIn, isAuthor, validateFestival, catchAsync(festivals.updateFestival))
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(festivals.deleteFestival))
 
 module.exports = router;
